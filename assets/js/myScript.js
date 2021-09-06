d221c16e3bd7e9c8839b4e026e2639112071c5ce
Luke Bailey <baileyl1991@gmail.com>
2021-09-06 09:07:25 +0000

    functions for gallery slideshow

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -12,4 +12,25 @@
 
     document.getElementById("greeting").innerHTML = 'Hello and ' + greet;
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var photos = document.getElementsByClassName("photo");
+  
+  if (n > photos.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = photos.length}
+  for (i = 0; i < photos.length; i++) {
+      photos[i].style.display = "none";  
+  }
+  
+  photos[slideIndex-1].style.display = "block";  
+  
+}
+
 
