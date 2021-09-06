c19a6c5d4b7c18c2a4fc1154571ddd4ed8977409
Luke Bailey <baileyl1991@gmail.com>
2021-09-06 19:18:03 +0000

    init map, custom markers

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -34,3 +34,35 @@ function showSlides(n) {
 }
 
 
+var locations = [
+    ['<h2>Caerphilly</h2>' + '<p>Length: 1 mile</p>',51.578602, -3.218060, 4],
+    ['<h2>Tonypandy</h2>' + '<p>Length: 2 mile</p>', 51.618408, -3.454590, 5],
+    ['<h2>Nantgarw</h2>' + '<p>Length: 3 mile</p>', 51.562470, -3.261900, 3],
+  ];
+
+
+function initMap() {
+let map = new google.maps.Map(document.getElementById("map"), {
+  center: { lat: 51.601150, lng: -3.349900 },
+  zoom: 10,
+});
+
+var infowindow = new google.maps.InfoWindow();
+
+  var marker, x;
+
+  for (x = 0; x < locations.length; x++) {  
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[x][1], locations[x][2]),
+      map: map
+    });
+
+    google.maps.event.addListener(marker, 'click', (function(marker, x) {
+      return function() {
+        infowindow.setContent(locations[x][0]);
+        infowindow.open(map, marker);
+        
+      }
+    })(marker, x));
+  }
+}
