22869f32e6bb443d4275ad53f658ff2612bd4f23
Luke Bailey <baileyl1991@gmail.com>
2021-09-09 16:55:55 +0000

    add all comments needed along with credits due

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,31 +1,41 @@
-var locations = [
-    ['<h2>The Rocking Stone</h2>' + '<p>Time: 1 hour</p>',51.60233, -3.32738],
-    ['<h2>Saint Gwynno Forest</h2>' + '<p>Time: 5 hours</p>', 51.65761, -3.41281],
-    ['<h2>Clydach Vale Park</h2>' + '<p>Time: 1hr 30mins</p>', 51.62471, -3.46850],
-    ['<h2>Eglwysilan Mountain</h2>' + '<p>Time: 1hr 45mins</p>', 51.60858, -3.30968],
-    ['<h2>Forest Fawr Park</h2>' + '<p>Time: 1 hour</p>', 51.54742, -3.23779],
-    ['<h2>Caerphilly Mountain</h2>' + '<p>Time: 1hr 30mins</p>', 51.56244, -3.22384],
-    ['<h2>Barry Sidings</h2>' + '<p>Time: 2hrs 30mins</p>', 51.60835, -3.37174],
-    ['<h2>Taff Trail</h2>' + '<p>Time: 2 hours</p>', 51.59341, -3.32003]
-  ];
+/* Google Map API
+    Using the support from documentation
+    https://developers.google.com/maps/documentation */
 
+  // Create array to store information
+  // Gathered latitude and longitude using Google Map
+  var locations = [
+      ['<h2>The Rocking Stone</h2>' + '<p>Time: 1 hour</p>',51.60233, -3.32738],
+      ['<h2>Saint Gwynno Forest</h2>' + '<p>Time: 5 hours</p>', 51.65761, -3.41281],
+      ['<h2>Clydach Vale Park</h2>' + '<p>Time: 1hr 30mins</p>', 51.62471, -3.46850],
+      ['<h2>Eglwysilan Mountain</h2>' + '<p>Time: 1hr 45mins</p>', 51.60858, -3.30968],
+      ['<h2>Forest Fawr Park</h2>' + '<p>Time: 1 hour</p>', 51.54742, -3.23779],
+      ['<h2>Caerphilly Mountain</h2>' + '<p>Time: 1hr 30mins</p>', 51.56244, -3.22384],
+      ['<h2>Barry Sidings</h2>' + '<p>Time: 2hrs 30mins</p>', 51.60835, -3.37174],
+      ['<h2>Taff Trail</h2>' + '<p>Time: 2 hours</p>', 51.59341, -3.32003]
+    ];
 
+// Function to initiate map with custom latitude and longitude with a zoom level of 11
 function initMap() {
 let map = new google.maps.Map(document.getElementById("map"), {
   center: { lat: 51.601150, lng: -3.349900 },
   zoom: 11,
 });
 
+// Create an pop up info window
 var infowindow = new google.maps.InfoWindow();
 
-  var marker, i;
+// Array to store Marker
+var marker, i;
 
-  for (i = 0; i < locations.length; i++) {  
+// iterate to store custom markers
+for (i = 0; i < locations.length; i++) {  
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(locations[i][1], locations[i][2]),
       map: map
     });
 
+    // Event listener from the user, when clicked on a marker, a pop up window displays
     google.maps.event.addListener(marker, 'click', (function(marker, i) {
       return function() {
         infowindow.setContent(locations[i][0]);
@@ -36,9 +46,16 @@ var infowindow = new google.maps.InfoWindow();
   }
 }
 
+/* Expanded text box to reveal more text
+    Using support from W3 Schools tutorial
+    https://www.w3schools.com/howto/howto_js_collapsible.asp */
+
+// Store variable    
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
+// Event listener for the user click on a route they like to know more.
+// Once clicked, the text will expand to reveal more text
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,34 +1,52 @@
 
+// Greeting Script
+/* This script gather the time of the day where the user is
+    and reply a message depending on the time of the day 
+    Use the support from 'Javascript and jQuery' book by Jon Duckett */
+
+    // Get time
     var time = new Date().getHours();
     var greet;
 
+    // If time is under 12, then reply 'Good Morning'
     if (time < 12) {
         greet = "Good Morning!";
-    } else if (time > 18) {
+    } 
+    // If time is greater than 18, then reply 'Good Evening'
+    else if (time > 18) {
         greet = "Good Evening!";
-    } else {
+    } 
+    // Otherwise reply 'Good Afternoon'
+    else {
         greet = "Good Afternoon!";
     }
 
+    // Send a message to id 'greeting' in home page
     document.getElementById("greeting").innerHTML = 'Hello and ' + greet;
 
-var slideIndex = 1;
-showSlides(slideIndex);
+    /* Gallery Slideshow
+        Using the support of W3 Schools Tutorial
+        https://www.w3schools.com/howto/howto_js_slideshow_gallery.asp  */
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+    // Variable to store images
+    var slideIndex = 1;
+    showSlides(slideIndex);
 
-function showSlides(n) {
-  var i;
-  var photos = document.getElementsByClassName("photo");
-  
-  if (n > photos.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = photos.length}
-  for (i = 0; i < photos.length; i++) {
-      photos[i].style.display = "none";  
-  }
-  
-  photos[slideIndex-1].style.display = "block";  
-  
-}
+    function plusSlides(n) {
+    showSlides(slideIndex += n);
+    }
+
+    // Create a function to iterate through images
+    function showSlides(n) {
+    var i;
+    var photos = document.getElementsByClassName("photo");
+    
+    if (n > photos.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = photos.length}
+    for (i = 0; i < photos.length; i++) {
+        photos[i].style.display = "none";  
+    }
+    
+    photos[slideIndex-1].style.display = "block";  
+    
+    }
